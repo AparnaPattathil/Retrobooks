2ab4e59d83e4fa362adc18910be28655b2f7df6c
Aparna Pattathil <aparna@qixxit.de>
2019-12-04 11:04:54 +0100

    toggle the visibility of progress bar

--- app/src/main/java/com/example/android/retrobooks/MainActivity.kt
+++ app/src/main/java/com/example/android/retrobooks/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.android.retrobooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        click_me_button.setOnClickListener {
+            Toast.makeText(this,"clicked",Toast.LENGTH_LONG).show()
+            if(loading.visibility == View.GONE){
+                loading.visibility =   View.VISIBLE
+            }else{
+                loading.visibility =  View.GONE
+            }
+        }
     }
 }
